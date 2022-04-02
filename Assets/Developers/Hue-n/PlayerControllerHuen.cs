398cb174745e12c3a3ea9e112aab9de4af2c7e9e
Hue-n <72900340+Hue-n@users.noreply.github.com>
2022-04-02 13:09:15 -0400

    BasePlayerController

---
+++ Assets/Developers/Hue-n/PlayerControllerHuen.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerHuen : MonoBehaviour
+{
+    bool canInput = true;
+
+    public Transform cam;
+
+    public float speed = 3;
+    public float sensitivity = 3;
+    public float jumpForce = 3;
+
+    private Rigidbody rb;
+
+
+    float verticalRotation = 0;
+    float horizontalRotation = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (canInput)
+        {
+            CalculateMovement();
+            HandleRotation();
+            Cursor.lockState = CursorLockMode.Locked;
+        }
+        else
+        {
+            Cursor.lockState = CursorLockMode.None;
+        }
+    }
+
+    //void InterpretGameManager(GameCommands command)
+    //{
+    //    switch (command)
+    //    {
+    //        case GameCommands.dialogue:
+    //            canInput = false;
+    //            break;
+    //        case GameCommands.exitDialogue:
+    //            canInput = true;
+    //            break;
+    //        case GameCommands.transition:
+    //            break;
+    //    }
+    //}
+
+    void CalculateMovement()
+    {
+        float moveX = Input.GetAxisRaw("Horizontal");
+        float moveZ = Input.GetAxisRaw("Vertical");
+
+        Vector2 userInput = new Vector2(moveX, moveZ).normalized;
+        Vector3 forwardConversion = (transform.right * userInput.x) + (transform.forward * userInput.y);
+        forwardConversion *= speed;
+
+        Vector3 moveDir = new Vector3(forwardConversion.x, rb.velocity.y, forwardConversion.z);
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            moveDir += transform.up * jumpForce;
+        }
+
+        rb.velocity = moveDir;
+    }
+
+    void HandleRotation()
+    {
+        Vector2 lookDir = new Vector2(Input.GetAxis("Mouse X"), Input.GetAxis("Mouse Y"));
+
+        verticalRotation += (-lookDir.y) * sensitivity;
+        horizontalRotation += lookDir.x * sensitivity;
+
+        verticalRotation = Mathf.Clamp(verticalRotation, -90, 90);
+
+        transform.localRotation = Quaternion.Euler(0, horizontalRotation, 0);
+        cam.localRotation = Quaternion.Euler(verticalRotation, cam.localRotation.y, cam.localRotation.z);
+    }
+}
---
+++ Assets/Import/CharacterCreatorTest.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterCreatorTest : MonoBehaviour
+{
+    public static CharacterCreatorTest instance;
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else if (instance != this)
+        {
+            Debug.Log("Instance already exists, destroying object!");
+            Destroy(this);
+        }
+    }
+
+    public GameObject male;
+    public GameObject female;
+
+    public GameObject activeObject;
+
+    public Color color1 = Color.white;
+    [HideInInspector] public float offset = 0.25f;
+
+    Color gray = Color.gray;
+
+    public bool _sex = true;
+
+    [Space]
+    public RectTransform _texture;
+    public Texture2D _refSprite;
+
+    public void OnClickPickerColor()
+    {
+        SetColor();
+    }
+
+    private void SetColor()
+    {
+        Vector3 imagePos = _texture.position;
+        float globalPosX = Input.mousePosition.x - imagePos.x;
+        float globalPosY = Input.mousePosition.y - imagePos.y;
+
+        int localPosX = (int)(globalPosX * (_refSprite.width / _texture.rect.width));
+        int localPosY = (int)(globalPosY * (_refSprite.height / _texture.rect.width));
+
+        Color c = _refSprite.GetPixel(localPosX, localPosY);
+        SetActualColor(c);
+    }
+
+    void SetActualColor(Color c)
+    {
+        color1 = c;
+        activeObject.GetComponentsInChildren<SkinnedMeshRenderer>()[1].material.color = color1;
+        activeObject.GetComponentsInChildren<SkinnedMeshRenderer>()[0].material.color = color1 * 0.25f;
+    }
+
+    public void SetSex(bool sex)
+    {
+        if (sex)
+        {
+            male.SetActive(true);
+            activeObject = male;
+            female.SetActive(false);
+        }
+        else
+        {
+            male.SetActive(false);
+            activeObject = female;
+            female.SetActive(true);
+        }
+
+        _sex = sex;
+        activeObject.GetComponentsInChildren<SkinnedMeshRenderer>()[1].material.color = color1;
+        activeObject.GetComponentsInChildren<SkinnedMeshRenderer>()[0].material.color = color1 * 0.25f;
+    }
+}
